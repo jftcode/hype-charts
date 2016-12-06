e1c0716644d0661502dd4e05d02d49c95b280846
Jason <jftcode@gmail.com>
2016-12-06 11:35:56 -0800

    Fetch sitename

--- scrape.js
+++ scrape.js
@@ -17,13 +17,15 @@ request("http://hypem.com/popular/week:Nov-14-2016?count=50", (err, resp, body) 
     let title = $(this).find('.track_name a.track span.base-title').text();
     let dateposted = $(this).find('.post_info a.readpost').attr('title');
     let siteid = $(this).find('.post_info a.follow-pill').attr('class').split(' ')[1].slice(9);
+    let sitename = $(this).find('.post_info a.blog-fav-off').text();
 
     console.log(
       'item: '+itemId,
       'artist: '+artist,
       'title: '+title,
       'dateposted: '+dateposted,
-      'siteid: '+siteid
+      'siteid: '+siteid,
+      'sitename: '+sitename
     );
   });
 
