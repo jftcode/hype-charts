6cf11f09fd1d48f84ef0626222c0b9d9d5afc82b
Jason <jftcode@gmail.com>
2016-12-06 10:48:35 -0800

    Fetch title

--- scrape.js
+++ scrape.js
@@ -14,9 +14,11 @@ request("http://hypem.com/popular/week:Nov-14-2016?count=50", (err, resp, body) 
   $("[data-itemid]").each(function(){
     let itemId = $(this).data().itemid;
     let artist = $(this).find('.track_name a.artist').text();
+    let title = $(this).find('.track_name a.track span.base-title').text();
     console.log(
       'item: '+itemId,
-      'artist: '+artist
+      'artist: '+artist,
+      'title: '+title
     );
   });
 
